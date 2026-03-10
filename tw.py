9a46133c67218454e594ba19f03d217d59f2d84d
whitesnakeftw <almcahm@hotmail.com>
2026-03-10 01:06:39 +0100

    improve tw

--- tw.py
+++ tw.py
@@ -1,39 +1,45 @@
-from pathlib import Path
 import streamlink
 import requests
 import re
+import time
+import traceback
 from utils import USER_AGENT
 
-OUTFILE = Path("twitch.m3u8")
+OUTFILE = "twitch.m3u8"
 IPHONE_UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Mobile/15E148 Safari/604.1"
 GROUP_TITLE = "TWITCH"
+MAX_RETRIES = 10
 
 twitch_profiles = [
-    "https://www.twitch.tv/grenbaud",
-    "https://www.twitch.tv/therealmarzaa",
-    "https://www.twitch.tv/lollolacustre",
-    "https://www.twitch.tv/tumblurr",
-    "https://www.twitch.tv/gioee",
-    "https://www.twitch.tv/kingsleague_it",
+    "https://www.twitch.tv/ishowspeed",
     "https://www.twitch.tv/kingsleague",
-    "https://www.twitch.tv/ilrossopiubelloditwitch",
-    "https://www.twitch.tv/none_cerbero_podcast",
+    "https://www.twitch.tv/kingsleague_it",
+    "https://www.twitch.tv/halo",
     "https://www.twitch.tv/naded",
     "https://www.twitch.tv/tylenul",
-    "https://www.twitch.tv/halo",
-    "https://www.twitch.tv/patrizio_official",
+    "https://www.twitch.tv/none_cerbero_podcast",
+    "https://www.twitch.tv/grenbaud",
+    "https://www.twitch.tv/lollolacustre",
+    "https://www.twitch.tv/gioee",
+    "https://www.twitch.tv/tumblurr",
+    "https://www.twitch.tv/ilrossopiubelloditwitch",
+    "https://www.twitch.tv/therealmarzaa",
     "https://www.twitch.tv/matteohs",
     "https://www.twitch.tv/marcomerrino",
-    "https://www.twitch.tv/ishowspeed",
     "https://www.twitch.tv/andreadel1988",
+    "https://www.twitch.tv/patrizio_official",
 ]
 
 
 def grab_profile_image(twitch_url):
-    response = requests.get(twitch_url, headers={"User-Agent": USER_AGENT}).text
-    match = re.search(r'content="([^"]+?-profile_image-[^"]+?)"', response)
-    if match:
-        return match.group(1)
+    for i in range(0, MAX_RETRIES):
+        response = requests.get(twitch_url, headers={"User-Agent": USER_AGENT}).text
+        match = re.search(r'content="([^"]+?-profile_image-[^"]+?)"', response)
+        if match:
+            return match[1]
+        else:
+            i += 1
+            time.sleep(1)
     return ""
 
 
@@ -46,7 +52,8 @@ def get_stream_url(twitch_url):
         else:
             return None
     except Exception as e:
-        print(f"Error {twitch_url}: {e}")
+        print(f'(tw {twitch_url}) {e.__class__.__module__}.{e.__class__.__name__}: {e}')
+        traceback.print_exc()
         return None
 
 
